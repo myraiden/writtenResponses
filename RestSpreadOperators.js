e605ff50b660d7725d9ae5693aec9a6aac257b94
myraiden <klj3604@gmail.com>
2024-07-22 23:51:23 -0400

    added index page to run code, and added functions

--- RestSpreadOperators.js
+++ RestSpreadOperators.js
@@ -8,6 +8,9 @@
 
 // // **REFACTOR:**
 // /* Write an ES2015 Version */
+function filterOutOdds(...allNums){
+    return allNums.filter((a)=>a%2!==0);
+};
 
 // /******************************************************************************************************/
 // // ## **findMin**
@@ -42,26 +45,26 @@ function findMin(...nums){
 // // **GIVEN:**
 // /** remove a random element in the items array
 // and return a new array without that item. */
-// function removeRandom(items) {
-// }
+function removeRandom(items) {
+}
 
 // /** Return a new array with every item in array1 and array2. */
-// function extend(array1, array2) {
-// }
+function extend(array1, array2) {
+}
 
 // /** Return a new object with all the keys and values
 // from obj and a new key/value pair */
-// function addKeyVal(obj, key, val) {
-// }
+function addKeyVal(obj, key, val) {
+}
 
 // /** Return a new object with a key removed. */
-// function removeKey(obj, key) {
-// }
+function removeKey(obj, key) {
+}
 
 // /** Combine two objects and return a new object. */
-// function combine(obj1, obj2) {
-// }
+function combine(obj1, obj2) {
+}
 
 // /** Return a new object with a modified key and value. */
-// function update(obj, key, val) {
-// }
+function update(obj, key, val) {
+}
