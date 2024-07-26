57854ed108c13bbd1039129fbd07f85238cf2475
myraiden <klj3604@gmail.com>
2024-07-26 01:17:09 -0400

    all functions work but need to mod last funcs to arrowFunc

--- RestSpreadOperators.js
+++ RestSpreadOperators.js
@@ -31,12 +31,22 @@ function findMin(...nums){
 // // Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 // // mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
 
+function mergeObjects(obj1,obj2){
+    return {...obj1,...obj2};
+};
 
 // /******************************************************************************************************/
 // // ## **doubleAndReturnArgs**
 // // Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
 // doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
 // doubleAndReturnArgs([2],10,4) // [2, 20, 8]
+function doubleAndReturnArgs(originalArray,...allArgs){
+    let doubleArgs=allArgs.reduce(function(accumArg,currArg){
+        return [...accumArg, (currArg*2)];
+    },[]);
+    return [...originalArray,...doubleArgs];
+};
+
 
 // /******************************************************************************************************/
 // // ## **Slice and Dice!**
@@ -45,26 +55,40 @@ function findMin(...nums){
 // // **GIVEN:**
 // /** remove a random element in the items array
 // and return a new array without that item. */
+//test: removeRandom(['green','yellow','banana','rabbit','cat','turtle','ship','car','plane'])
 function removeRandom(items) {
+    let randomIndex=Math.round(Math.random()*items.length);
+    return items.filter((currVal,currentIndex,arrItems)=> currentIndex!=randomIndex); 
 }
 
 // /** Return a new array with every item in array1 and array2. */
+//test: extend([1,2,3,4],[9,8,7,6])
 function extend(array1, array2) {
+    return [...array1,...array2];
 }
 
 // /** Return a new object with all the keys and values
 // from obj and a new key/value pair */
+//test: addKeyVal({tires:4, doors:2, spareTire:0},'car','mitsubishi')
 function addKeyVal(obj, key, val) {
+    return {...obj, [key]:val};
 }
 
 // /** Return a new object with a key removed. */
+//test: removeKey({flavor:'blueberry',color:'purple',plant:'fruit',organic:'true'},'organic')
 function removeKey(obj, key) {
+    let {[key]:value,...restObj}=obj;
+    return restObj;
 }
 
 // /** Combine two objects and return a new object. */
+// // combine({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
 function combine(obj1, obj2) {
+    return {...obj1,...obj2};
 }
 
 // /** Return a new object with a modified key and value. */
+//test: update({flavor:'blueberry',color:'purple',plant:'fruit',organic:'true'}, 'organic', 'false')
 function update(obj, key, val) {
+    return {...obj,[key]:val};
 }
